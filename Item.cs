bde4e6def1a19f905c9ca316fe5ff9c33e32e91c
thebotfly <69916612+thebotfly@users.noreply.github.com>
2020-09-22 18:08:52 -0500

    Added item class

---
+++ Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Item
+    {
+        private int _damage;
+        public Item(int damage)
+        {
+            _damage = damage;
+        }
+        public int GetDamage()
+        {
+            return _damage;
+        }
+    }
+}
